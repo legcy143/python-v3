ac26ca378ee411165c0b64d694c16dcf48b8e0ea
legcy <legcy143@gmail.com>
2025-01-30 04:56:48 +0000

    init

---
+++ cobra_v_03/basic_01.py
@@ -0,0 +1,18 @@
+a = 6;
+b = False;
+name = input("Enter your name : ")
+
+arr =["volvo" , "bmw" , "ferrari"]
+
+print("\nhello : ",name)
+print("type of input is : ",type(name))
+print("type of a is : ",type(a))
+print("type of b is : ",type(b))
+
+print(arr)
+print(arr[0])
+print(arr.__len__())
+# print(arr[220])
+
+
+print("\n End \n")
